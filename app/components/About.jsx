f728563ec224764acebec06abdba9f2c06802e98
Toby J. Henderson <bigtob@gmail.com>
2017-08-15 19:08:49 -0500

    Finished Styling About Page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,20 @@
 var React = require('react');
 
 var About = (props) => {
-  return (  
+  return (
     <div>
       <h1 className="text-centered">About</h1>
-      <p className="text-centered">This weather application was created with the React Library</p>
+      <p>This weather application was created with the React Library.</p>
+      <ul>
+        <li>
+          <a href="http://facebook.github.io/react">React</a> - this was the
+          Javascript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used Open
+          Weather Map to search for data by city name.
+        </li>
+      </ul>
     </div>
 
   )
