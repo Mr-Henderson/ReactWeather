667877fd74fad72256f52a0369a0e5da5fe2ff95
Toby J. Henderson <bigtob@gmail.com>
2017-08-16 06:11:02 -0500

    bug fix error modal

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -5,9 +5,10 @@ const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?app
 module.exports = {
   getTemp: function (location) {
     var encodedLocation = encodeURIComponent(location);
-    var requestUrl =`${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
-    return axios.get(requestUrl).then(function (res){      
+
+    return axios.get(requestUrl).then(function (res){
       if (res.data.cod && res.data.message) {
         throw new Error(res.data.message);
       } else {
